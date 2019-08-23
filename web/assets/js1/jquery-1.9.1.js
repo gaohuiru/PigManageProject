67cbd7468801911434f22a38673703e601c812f6
binbinbin666 <1426308204@qq.com>
2019-08-23 17:09:48 +0800

    抽出头部和左部模板

--- web/assets/js1/jquery-1.9.1.js
+++ web/assets/js1/jquery-1.9.1.js
@@ -7236,7 +7236,7 @@ jQuery(function() {
 	// getComputedStyle returns percent when specified for top/left/bottom/right
 	// rather than make the css module depend on the offset module, we just check for it here
 	if ( !jQuery.support.pixelPosition && jQuery.fn.position ) {
-		jQuery.each( [ "top", "left" ], function( i, prop ) {
+		jQuery.each( [ "top", "left" ], function(i, prop ) {
 			jQuery.cssHooks[ prop ] = {
 				get: function( elem, computed ) {
 					if ( computed ) {
