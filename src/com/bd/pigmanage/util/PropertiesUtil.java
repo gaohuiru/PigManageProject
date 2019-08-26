837f131dd6cec8202692bac17362bb97d05c8f08
AlwaysXu <1205817366@qq.com>
2019-08-26 11:46:05 +0800

    识别properties文件不够聪明

--- src/com/bd/pigmanage/util/PropertiesUtil.java
+++ src/com/bd/pigmanage/util/PropertiesUtil.java
@@ -12,19 +12,20 @@ public class PropertiesUtil {
     }
 
     //获取properties文件对应的值
-    public static String getValue(String path, String key) throws Exception {
+    public String getValue(String path, String key) throws Exception {
         //加载properties的文件内容
         Properties pro = new Properties();
         // 使用InPutStream流读取properties文件
-        InputStream is = ClassLoader.getSystemResourceAsStream(path);
-        System.out.println(is);
+//        InputStream is = this.getClass().getClassLoader().getSystemResourceAsStream(path);
+//        System.out.println(is);
 
 
-//        BufferedReader bufferedReader = new BufferedReader(
-//                new FileReader(path)
+
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(path));
         //加载配置文件
         try {
-            pro.load(is);
+            pro.load(bufferedReader);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/com/bd/pigmanage/util/TableUtil.java
+++ src/com/bd/pigmanage/util/TableUtil.java
@@ -5,15 +5,17 @@ package com.bd.pigmanage.util;
  */
 public class TableUtil {
     //获取当前下项目的在此电脑上的路径
-    //获取物理表需访问的配置文件路径
-    private final static String PATH= "DataTable.properties";
+    //获取物理表需访问的配置文件路路径
+    private final static String PATH= "D:\\javahome\\PigManageProject\\resource\\DataTable.properties";
+    private final static String PATHs="DataTable.properties";
     public TableUtil(){
 
     }
 
     //获取对应的物理表
     public static String[] getTables(String key) throws Exception {
-        String value=PropertiesUtil.getValue(PATH,key);
+        PropertiesUtil pro=new PropertiesUtil();
+        String value=pro.getValue(PATH,key);
         return value.split("\\.");
 
     }
