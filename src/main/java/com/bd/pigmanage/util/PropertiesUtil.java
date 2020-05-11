d15679547493a47e95ef6f4df1f7190489b0c339
XuBin8866 <1205817366@qq.com>
2020-05-11 16:10:11 +0800

    解决了读取properties文件的问题

--- src/main/java/com/bd/pigmanage/util/PropertiesUtil.java
+++ src/main/java/com/bd/pigmanage/util/PropertiesUtil.java
@@ -8,37 +8,31 @@ import java.util.Properties;
 
 /**
  * 读取配置文件的工具类
+ *
  * @author AlwaysXu
  */
 public class PropertiesUtil {
     public static void main(String[] args) throws Exception {
-        PropertiesUtil propertiesUtil=new PropertiesUtil();
-        getValue(null,null);
+       getValue(null, null);
     }
 
     /**
      * TODO 需要读取配置文件逻辑，不需要提前去找配置文件位置
      * 获取properties文件对应的值
-     * @param path 配置文件路径
+     *
+     * @param propertiesName 配置文件名称
      * @param key
      * @return
      * @throws Exception
      */
-    public static String getValue(String path, String key) throws Exception {
-        // 使用InPutStream流读取properties文件(找不到path文件，返回is为空)
-        // 莫名奇妙
-        // InputStream is = Properties.class.getClassLoader().getResourceAsStream("DataTable.properties");
-        // URL resource = PropertiesUtil.class.getClassLoader().getResource("DataTable.properties");
-        // System.out.println(resource);
-
+    public static String getValue(String propertiesName, String key) throws Exception {
+        // 之前一直使用：PropertiesUtil.class.getClassLoader().getSystemResourceAsStream(path)导致获取不到配置文件
+        InputStream is = PropertiesUtil.class.getClassLoader().getResourceAsStream(propertiesName);
+        System.out.println(is);
         //加载properties的文件内容
         Properties pro = new Properties();
-        //使用BufferedReader和FileReader读取
-        BufferedReader bufferedReader = new BufferedReader(
-                new FileReader(path));
-        //加载配置文件
         try {
-            pro.load(bufferedReader);
+            pro.load(is);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/bd/pigmanage/util/TableUtil.java
+++ src/main/java/com/bd/pigmanage/util/TableUtil.java
@@ -2,34 +2,26 @@ package com.bd.pigmanage.util;
 
 /**
  * 读取视图表对应的物理表的工具类
+ *
  * @author AlwaysXu
  */
 public class TableUtil {
     //properties配置文件在项目中的相对位置
-    private final static String RELATIVEPATH="DataTable.properties";
-
-    public TableUtil(){
-
-    }
+    private final static String RELATIVEPATH = "DataTable.properties";
 
     /**
      * 获取对应的物理表
+     *
      * @param key
      * @return
      * @throws Exception
      */
     public static String[] getTables(String key) throws Exception {
-        //获取该类的路径，用于截取出项目路径以适配不同的电脑
-        //path不以’/'开头时，默认是从此类所在的包下取资源；
-       // path  以’/'开头时，则是从ClassPath根下获取；
-        String absolutePath=TableUtil.class.getClassLoader().getResource("").getPath().substring(1);
-        System.out.println("绝对路径"+absolutePath);
-        //将项目路径和配置文件的相对路径组合
-        String path=absolutePath+RELATIVEPATH;
-        System.out.println("组合后的路径: "+path);
-//        读取配置文件中值
-        String value=PropertiesUtil.getValue(path,key);
-//        返回存有物理表的字符串数数组
+        //读取配置文件中值
+        String value = PropertiesUtil.getValue(RELATIVEPATH, key);
+        //返回存有物理表的字符串数数组
         return value.split("\\.");
     }
+
+
 }
